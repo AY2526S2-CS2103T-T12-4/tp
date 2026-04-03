145cbc627b6db52ab5bc9026124a87a4dee49d2a
yueqqq <yueqianhong@hotmail.com>
2026-04-04 06:51:06 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/model/order/ActiveOrderInRegionPredicateTest.java
+++ src/test/java/seedu/address/model/order/ActiveOrderInRegionPredicateTest.java
@@ -1,7 +1,7 @@
 package seedu.address.model.order;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
@@ -59,4 +59,3 @@ public class ActiveOrderInRegionPredicateTest {
         assertEquals(predicate.hashCode(), samePredicate.hashCode());
     }
 }
-
