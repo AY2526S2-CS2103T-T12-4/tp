6e0afed624c22414900ed0966a49372d021e097d
Achiack <70217903+Achiack@users.noreply.github.com>
2026-04-09 16:50:39 +0800

    Update src/main/java/seedu/address/logic/commands/order/CompleteOrderCommand.java
    Co-authored-by: yellowbrickkode <kyrachoo28@gmail.com>

--- src/main/java/seedu/address/logic/commands/order/CompleteOrderCommand.java
+++ src/main/java/seedu/address/logic/commands/order/CompleteOrderCommand.java
@@ -17,7 +17,7 @@ import seedu.address.model.order.OrderMap;
 public class CompleteOrderCommand extends Command {
     public static final String COMMAND_WORD = "complete";
 
-    public static final String MESSAGE_SUCCESS = "Order %d marked as completed for %s.";
+    public static final String MESSAGE_SUCCESS = "Order %d for %s marked as completed.";
 
     public static final String MESSAGE_INVALID_INDEX = "No order with that index was found";
     public static final String MESSAGE_USAGE = COMMAND_WORD
